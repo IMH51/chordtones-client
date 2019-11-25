22141a4e401b956ff5f6845ebb402ab05fc6df22
IMH51 <ianmartinharrison@outlook.com>
2019-11-25 16:38:37 +0000

    added api and guitar data helpers

---
+++ src/helpers/guitarData.js
@@ -0,0 +1,5 @@
+const strings = [1,2,3,4,5,6]
+const frets = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24]
+const markers = [3,5,7,9,12,15,17,19,21,24]
+
+export { strings, frets, markers }
