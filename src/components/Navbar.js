6987dba4f842c503adaa3c7c540ae951349c4ef4
IMH51 <ianmartinharrison@outlook.com>
2019-11-25 16:39:25 +0000

    added helpers to navbar, refactored

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,11 +3,10 @@ import './Navbar.css'
 import { Link } from 'react-router-dom'
 
 const Navbar = ({ page, clearFretboard, logout, username}) => {
-  const navText = `${page[0].toUpperCase()}${page.slice(1)}`
   return (
     <div className="nav_container">
       <ul>
-        <li><Link className="button-link" to={`/${page}`}><button onClick={clearFretboard}>{navText}</button></Link></li>
+        <li><Link className="button-link" to={`/${page.toLowerCase()}`}><button onClick={clearFretboard}>{page}</button></Link></li>
         <li><img className='navbar-logo' src="/assets/chordtones-icon-black.png" alt="chordtones-logo" /><h3 className="navbar-logo-text">chordTones</h3></li>
         <li><button onClick={logout}>Logout {username}</button></li>
       </ul>
