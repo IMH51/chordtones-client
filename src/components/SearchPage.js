3c8272d9c01846eb0e5a7fea4695975e854b211d
IMH51 <ianmartinharrison@outlook.com>
2019-11-25 16:39:36 +0000

    added helpers to searchpage, refactored

--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -13,19 +13,19 @@ class SearchPage extends Component {
   }
 
   render() {
-    const { username, logout, clearFretboard, page, chord, query, strings, frets, changeFretNumber, getChordName } = this.props
+    const { username, logout, clearFretboard, chord, query, changeFretNumber, getChordName } = this.props
     return (
       <div className="searchpage-container">
-        <Navbar {...{ username, logout, clearFretboard, page }}/>
+        <Navbar page="Collection" {...{ username, logout, clearFretboard, }}/>
         <div className="fretboard_wrapper">
           <div className="search-fretboard-container">
           <p className="instruction-text">
-            Select your frets (or 'x' for open strings) then click 'Get Chord Name' to get more information and save to your collection
+            Select your frets (or 'x' for open strings) then click 'Get Chord Name' to get more information and save to your collection.
           </p>
           <div className="cord-wrapper">
             <h1>{chord && chord.chord_name.trim()}</h1>
           </div>
-          <Fretboard {...{query, strings, frets, changeFretNumber, chord}}/>
+          <Fretboard {...{query, changeFretNumber, chord}}/>
           <div className="chord-button-container">
           <button className="get-chord-button" onClick={getChordName}>Get Chord Name</button>
           <button className="clear-fretboard-button" onClick={clearFretboard}>Clear Fretboard</button>
