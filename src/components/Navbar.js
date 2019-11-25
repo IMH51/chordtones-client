091d44f70974356775f3bbe129db168d3ff5c40b
IMH51 <ianmartinharrison@outlook.com>
2019-11-25 12:32:08 +0000

    refactored & destructured Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,16 +2,17 @@ import React from 'react';
 import './Navbar.css'
 import { Link } from 'react-router-dom'
 
-const Navbar = props => {
+const Navbar = ({ page, clearFretboard, logout, username}) => {
+  const navText = `${page[0].toUpperCase()}${page.slice(1)}`
   return (
     <div className="nav_container">
       <ul>
-        <li><Link className="button-link" to={`/${props.page}`}><button onClick={() => props.clearFretboard()}>{props.page[0].toUpperCase() + props.page.slice(1)}</button></Link></li>
+        <li><Link className="button-link" to={`/${page}`}><button onClick={clearFretboard}>{navText}</button></Link></li>
         <li><img className='navbar-logo' src="/assets/chordtones-icon-black.png" alt="chordtones-logo" /><h3 className="navbar-logo-text">chordTones</h3></li>
-        <li><button onClick={props.logout}>Logout {props.username}</button></li>
+        <li><button onClick={logout}>Logout {username}</button></li>
       </ul>
     </div>
   );
-};
+}
 
 export default Navbar;
