634aedbbb46a481537f492a7bc46042d728ada1e
IMH51 <ianmartinharrison@outlook.com>
2019-11-25 12:32:30 +0000

    refactored & destructured Fret

--- src/components/Fret.js
+++ src/components/Fret.js
@@ -1,17 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './Fret.css'
 
-class Fret extends Component {
-
-  render() {
-    return (
-      <div className="fret-container">
-        <div className="fret-display-container" >
-        <button className={`${this.props.buttonClass}`} style={this.props.stringValue === "X" && this.props.fret === 0 ? {backgroundImage: `url(${process.env.PUBLIC_URL + "./assets/x.svg"})`} : null} onClick={(event) => this.props.changeFretNumber(this.props.string, this.props.fret)}>{this.props.finger === "X" ? "" : this.props.finger}</button>
-        </div>
+const Fret = ({ buttonClass, stringValue, string, fret, changeFretNumber, finger}) => {
+  const style = (stringValue === "X" && fret === 0) ? {backgroundImage: `url(${process.env.PUBLIC_URL + "./assets/x.svg"})`} : {}
+  const onClick = e => changeFretNumber(string, fret)
+  const fingerText = finger !== "X" && finger
+  return (
+    <div className="fret-container">
+      <div className="fret-display-container" >
+        <button className={buttonClass} {...{style, onClick}}>{fingerText}</button>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 export default Fret;
