915c1602286cf38ce3d234a8625267f3e11c3b2e
IMH51 <ianmartinharrison@outlook.com>
2019-11-25 17:05:54 +0000

    added hooks to collection and search pages

--- src/components/CollectionPage.js
+++ src/components/CollectionPage.js
@@ -1,77 +1,69 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import './CollectionPage.css'
 
 import Navbar from './Navbar'
 import Fretboard from './Fretboard'
 import API from '../helpers/API';
 
-class CollectionPage extends Component {
-  
-  state = {
-      collection: [],
-      selectedChord: null
-    }
+const CollectionPage = ({ username, logout, clearFretboard, history}) => {
 
-  getUserChords = () => {
+  const [collection, setCollection] = useState([])
+  const [selectedChord, setSelectedChord] = useState(null)
+
+  const getUserChords = () => {
    API.get(API.userChordsUrl)
     .then(collection => {
         if (collection.error) {
           alert(collection.error)
         } else {
-        this.setState({ collection })
+        setCollection(collection)
       }
     })
   }
 
-  onChangeChord = i => {
-    this.setState({selectedChord: this.state.collection[i]})
-  }
+  const onChangeChord = i => setSelectedChord(collection[i])
 
-  handleChordDelete = id => {
+  const handleChordDelete = id => {
     API.destroy(API.chordsUrl, id)
       .then(collection => {
         if (collection.error) {
           alert(collection.error)
         } else {
           alert("Chord successfully deleted from your Collection!")
-          this.setState({selectedChord: null, collection })
+          setSelectedChord(null)
+          setCollection(collection)
         }
       })
   }
 
-  componentDidMount() {
-    if (!this.props.username) {
-      this.props.history.push('/')
-    } else {
-    this.getUserChords()
-    }
-  }
+  const onClickDelete = () => handleChordDelete(selectedChord.id)
 
-  render() {
-    const { collection, selectedChord } = this.state
-    const { username, logout, clearFretboard} = this.props
-    const {onChangeChord, handleChordDelete} = this
-    const onClickDelete = () => handleChordDelete(selectedChord.id)
-    return (
-      <div className="collectionpage-container">
-        <Navbar page='Search' {...{username, logout, clearFretboard}} />
-        <div className="collection-page-wrapper">
+  useEffect(() => {
+      if (!username) {
+        history.push('/')
+      } else {
+        getUserChords()
+      }
+    }, [])
+
+  return (
+    <div className="collectionpage-container">
+      <Navbar page='Search' {...{username, logout, clearFretboard}} />
+      <div className="collection-page-wrapper">
         <div className="collection_wrapper">
           <p className="instruction-text">To view a chord from your collection, select it from the dropdown menu</p>
           <select name="chord-select" className="chord-select" onChange={e => onChangeChord(e.target.value)}>
           <option>Select A Chord</option>
-          {collection.map((chord, i) => <option key={chord.id} value={i}>{chord.chord_name.trim()}</option>)}
-        </select>
-        <Fretboard {...{collection}} chord={selectedChord} query={selectedChord}/>
-        <div className="delete-button-container">
-        {selectedChord && <button className="get-chord-button" onClick={onClickDelete}>Delete Chord from Collection</button>}
-        </div>
-        </div>
+            {collection.map((chord, i) => <option key={chord.id} value={i}>{chord.chord_name.trim()}</option>)}
+          </select>
+          <Fretboard {...{collection}} chord={selectedChord} query={selectedChord}/>
+          <div className="delete-button-container">
+            {selectedChord && <button className="get-chord-button" onClick={onClickDelete}>Delete Chord from Collection</button>}
+          </div>
         </div>
       </div>
-    )
-  }
-
+    </div>
+  )
 }
 
 export default CollectionPage
--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -1,24 +1,16 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 
 import Fretboard from './Fretboard'
 import Navbar from './Navbar'
 import './SearchPage.css'
 
-class SearchPage extends Component {
-
-  componentDidMount() {
-    if (!this.props.username) {
-      this.props.history.push('/')
-    }
-  }
-
-  render() {
-    const { username, logout, clearFretboard, chord, query, changeFretNumber, getChordName } = this.props
-    return (
-      <div className="searchpage-container">
-        <Navbar page="Collection" {...{ username, logout, clearFretboard, }}/>
-        <div className="fretboard_wrapper">
-          <div className="search-fretboard-container">
+const SearchPage = ({ username, logout, clearFretboard, chord, query, changeFretNumber, getChordName, history }) => {
+  useEffect(() => !username && history.push('/'), [])
+  return (
+    <div className="searchpage-container">
+      <Navbar page="Collection" {...{ username, logout, clearFretboard, }}/>
+      <div className="fretboard_wrapper">
+        <div className="search-fretboard-container">
           <p className="instruction-text">
             Select your frets (or 'x' for open strings) then click 'Get Chord Name' to get more information and save to your collection.
           </p>
@@ -26,15 +18,14 @@ class SearchPage extends Component {
             <h1>{chord && chord.chord_name.trim()}</h1>
           </div>
           <Fretboard {...{query, changeFretNumber, chord}}/>
-          <div className="chord-button-container">
-          <button className="get-chord-button" onClick={getChordName}>Get Chord Name</button>
-          <button className="clear-fretboard-button" onClick={clearFretboard}>Clear Fretboard</button>
-          </div>
+           <div className="chord-button-container">
+            <button className="get-chord-button" onClick={getChordName}>Get Chord Name</button>
+            <button className="clear-fretboard-button" onClick={clearFretboard}>Clear Fretboard</button>
           </div>
         </div>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 export default SearchPage;
